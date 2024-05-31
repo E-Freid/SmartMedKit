63cf63e8422c5a766174149b86e4be7a48f02c16
Eli Freid <86243882+E-Freid@users.noreply.github.com>
2024-05-31 12:24:42 +0300

    updated name and location of project

---
+++ backend/Backend/Celery/utils.py
@@ -0,0 +1,14 @@
+import subprocess
+
+
+def start_celery():
+    worker = subprocess.Popen(['celery', '-A', 'Backend.Celery.celery_config', 'worker', '--loglevel=info'])
+    beat = subprocess.Popen(['celery', '-A', 'Backend.Celery.celery_config', 'beat', '--loglevel=info'])
+    return worker, beat
+
+
+def cleanup(worker, beat):
+    worker.terminate()
+    beat.terminate()
+    worker.wait()
+    beat.wait()
--- backend/SmartMedKit-Backend/Models/__init__.py
+++
@@ -1,6 +0,0 @@
-from Models.event import EventModel
-from Models.admin import AdminModel
-from Models.kit import KitModel
-from Models.kit_admin import KitAdminModel
-from Models.kit_compartment import KitCompartmentModel
-from Models.measurements import MeasurementsModel
