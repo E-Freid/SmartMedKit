e68f105c14083ea0b6daf6488e237d222de7c344
Eli Freid <86243882+E-Freid@users.noreply.github.com>
2024-05-31 12:12:59 +0300

    fixed some bugs

--- backend/SmartMedKit-Backend/Models/kit_admin.py
+++
@@ -1,7 +0,0 @@
-from db import db
-
-class KitAdminModel(db.Model):
-    __tablename__ = "kit_admin"
-
-    kit_id = db.Column(db.Integer, db.ForeignKey("kits.id"), primary_key=True)
-    admin_id = db.Column(db.Integer, db.ForeignKey("admins.id"), primary_key=True)
