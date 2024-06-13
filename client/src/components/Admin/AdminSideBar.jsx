35fa40b7e202a6d849b68baf5261dfbfd10bf6f8
gilmor7 <gilov1997@gmail.com>
2024-06-13 13:41:12 +0300

    Redesign the admin sidebar.

--- client/src/components/Admin/AdminSideBar.jsx
+++ client/src/components/Admin/AdminSideBar.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import {Navbar, Nav, Offcanvas} from 'react-bootstrap'
 import {NavLink} from 'react-router-dom';
 
+import '../../styles/sideBar.css';
+
 const AdminSideBar = () => {
   return (
     <>
         <Navbar.Toggle aria-controls='offcanvasNavbar'/>
         <Navbar.Offcanvas
           id='offcanvasNavbar'
+          className="sidebar"
           aria-labelledby='offcanvasNavbarLabel'
           placement='start'
-          variant='dark'
         >
           <Offcanvas.Header closeButton>
             <Offcanvas.Title id='NavbarLabel'>Admin Menu</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className='justify-content-end flex-grow-1 pe-3'>
-              <NavLink to="/admin/about">My Kits</NavLink>
-              <NavLink to="/admin/about">Add A Kit</NavLink>
-              <NavLink to="/admin/work">Dashboard</NavLink>
+            <Nav className="flex-column">
+              <NavLink to="/admin/kits" className="nav-link">My Kits</NavLink>
+              <NavLink to="/admin/dashboard" className="nav-link">Dashboard</NavLink>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
