5607a171ae814fd4bf2696e304f69b6e1f90bab8
gilmor7 <gilov1997@gmail.com>
2024-09-07 17:05:36 +0300

    Delete HomePage component and use the dashboard as home page. Adjust the sidebar and router component.

--- client/src/components/Admin/AdminRoutes.jsx
+++ client/src/components/Admin/AdminRoutes.jsx
@@ -3,7 +3,6 @@ import {Routes, Route, Navigate, useLocation} from 'react-router-dom';
 import { AuthProvider, useAuth } from '../../state/AuthContext';
 
 import AdminLoginPage from '../../pages/Admin/Login';
-import AdminHomePage from '../../pages/Admin/HomePage';
 import AddKitPage from "../../pages/Admin/AddKit";
 import Dashboard from "../../pages/Admin/Dashboard";
 import MyKits from "../../pages/Admin/MyKits";
@@ -19,13 +18,13 @@ const PrivateRoute = ({ component: Element, ...rest }) => {
 const AdminRoutes = () => (
   <AuthProvider>
     <Routes>
-      <Route path="/admin" element={<PrivateRoute component={AdminHomePage} />} />
+      <Route path="/admin" element={<PrivateRoute component={Dashboard} />} />
       <Route path="/admin/login" element={<AdminLoginPage />} />
       <Route path="/admin/kit/:kitId" element={<PrivateRoute component={KitPage} />} />
       <Route path="/admin/kits" element={<PrivateRoute component={MyKits} />} />
       <Route path="/admin/kits/add" element={<PrivateRoute component={AddKitPage} />} />
       <Route path="/admin/kits/add/:kitId" element={<PrivateRoute component={AddKitPage} />} />
-      <Route path="/admin/dashboard" element={<PrivateRoute component={Dashboard} />} />
+      {/*<Route path="/admin/dashboard" element={<PrivateRoute component={Dashboard} />} />*/}
     </Routes>
   </AuthProvider>
 );
